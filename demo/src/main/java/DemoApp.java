99dbff51e83f96ccda757d00be66c36b6594c543
junjie.fu_U+ <junjie.fu@united-imaging.com>
2022-05-21 16:01:57 +0800

    feat add demo

--- demo/src/main/java/DemoApp.java
+++ demo/src/main/java/DemoApp.java
@@ -1,5 +1,9 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.demo.domain.Student;
 
 /**
@@ -11,8 +15,16 @@ import org.springframework.demo.domain.Student;
  */
 public class DemoApp {
 	public static void main(String[] args) {
+		/*jdk动态代理class输出文件位置*/
+		System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+		/*cglib 生成的class文件目录*/
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "");
 		ApplicationContext context = new ClassPathXmlApplicationContext("demo-config.xml");
-		Student student = (Student)context.getBean("student");
+		Student student = (Student) context.getBean("student");
 		System.out.println("学生名称是：" + student.getName() + " 年龄是：" + student.getAge());
+
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("demo-config.xml"));
+		Student student1 = (Student) factory.getBean("student");
+		System.out.println("学生名称是：" + student1.getName() + " 年龄是：" + student1.getAge());
 	}
 }
